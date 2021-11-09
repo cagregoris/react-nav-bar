b5d8793c490e6773628e93b82d48e21dc7068bb4
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-11-08 17:30:37 -0700

    first dropdown done

---
+++ src/components/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItemsTours = [
+  {
+    title: 'Mountain',
+    path: '/mountain',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Heritage',
+    path: '/heritage',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Larger Group',
+    path: '/larger-group',
+    cName: 'dropdown-link'
+  },
+]
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,85 @@
 import React, { useState } from 'react';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
+import Button from './Button';
 import './Navbar.css';
-import { Dropdown } from './Dropdown';
+
+// Dropdowns
+import DropdownTours from './DropdownTours';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const [dropdown, setDropdown]= useState(false)
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const onMouseEnter = () => {
+    if(window.innerWidth < 960) {
+      setDropdown(false)
+    } else {
+      setDropdown(true)
+    }
+  };
+
+  const onMouseLeave = () => {
+    if(window.innerWidth < 960) {
+      setDropdown(false)
+    } else {
+      setDropdown(false)
+    }
+  }
+
   return (
     <>
       <nav className="navbar">
         <Link to='/' className='navbar-logo'>
           LOGO
         </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+              About
+            </Link>
+          </li>
+          <li className="nav-item"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link to='/tours' className='nav-links' onClick={closeMobileMenu}>
+              Tours <i className='fas fa-caret-down' />
+            </Link>
+            {dropdown && <DropdownTours />}
+          </li>
+          <li className="nav-item">
+            <Link to='/booking' className='nav-links' onClick={closeMobileMenu}>
+              Booking <i className='fas fa-caret-down' />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+              Contact <i className='fas fa-caret-down' />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/guest-comments' className='nav-links' onClick={closeMobileMenu}>
+              Guest Comments
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/login' className='nav-links-mobile' onClick={closeMobileMenu}>
+              Login
+            </Link>
+          </li>
+        </ul>
+        <Button />
       </nav>
     </>
   )
